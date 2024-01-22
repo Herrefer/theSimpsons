58f69f9f412c2ef9784208fac0061c2b65e53548
Fernando Herrera <herrera770@outlook.es>
2024-01-22 19:15:08 -0300

    se finalizó el maquetado del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import ComponenteFrase from "./components/ComponenteFrase";
 
 function App() {
   return (
-    
-  )
+      <main className="d-flex flex-column align-items-center">
+        <div className="text-center mb-1">
+          <img
+            className="img-fluid simponsLogo"
+            src="../assets/The_Simpsons_yellow_logo.svg"
+            alt="logo The Simpsons"
+          />
+        </div>
+        <ComponenteFrase></ComponenteFrase>
+        <Button variant="warning">Generar frase</Button>
+      </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ComponenteFrase.jsx
@@ -0,0 +1,11 @@
+
+const ComponenteFrase = () => {
+    return (
+        <article className="d-flex flex-row p-5 mb-2 contenedorFrasePersonaje">
+            <div><img className="imagenPersonaje img-fluid" src="../../assets/Robert_Terwilliger-removebg-preview.png" alt="Bob Patiño" /></div>
+            <div className="mt-5 ms-5"><p>"Una vez traté de matar al mejor amante del mundo, pero supe que hay muchas leyes contra el suicidio." - Bob Patiño</p></div>
+        </article>
+    );
+};
+
+export default ComponenteFrase;
