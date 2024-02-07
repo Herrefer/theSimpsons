d6199ca2d3bb6b493296ec132590e686a02ebb79
Fernando Herrera <herrera770@outlook.es>
2024-02-07 01:36:16 -0300

    se complementó la página con una API generadora de personajes y frases de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Button } from "react-bootstrap";
 import ComponenteFrase from "./components/ComponenteFrase";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async() => {
+    //hacer una peticion get a la API
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const datos = await respuesta.json();
+    console.log(datos[0]);
+    setPersonaje(datos[0])
+  };
   return (
-      <main className="d-flex flex-column align-items-center">
-        <div className="text-center mb-1">
-          <img
-            className="img-fluid simponsLogo"
-            src="../assets/The_Simpsons_yellow_logo.svg"
-            alt="logo The Simpsons"
-          />
-        </div>
-        <ComponenteFrase></ComponenteFrase>
-        <Button variant="warning">Generar frase</Button>
-      </main>
+    <main className="d-flex flex-column align-items-center">
+      <div className="text-center mb-1">
+        <img
+          className="img-fluid simponsLogo"
+          src="../assets/The_Simpsons_yellow_logo.svg"
+          alt="logo The Simpsons"
+        />
+      </div>
+      <ComponenteFrase personajeProps = {personaje}></ComponenteFrase>
+      <Button variant="warning" onClick={consultarAPI} >Generar frase</Button>
+    </main>
   );
 }
 
--- src/components/ComponenteFrase.jsx
+++ src/components/ComponenteFrase.jsx
@@ -1,11 +1,21 @@
-
-const ComponenteFrase = () => {
-    return (
-        <article className="d-flex flex-row p-5 mb-2 contenedorFrasePersonaje">
-            <div><img className="imagenPersonaje img-fluid" src="../../assets/Robert_Terwilliger-removebg-preview.png" alt="Bob Patiño" /></div>
-            <div className="mt-5 ms-5"><p>"Una vez traté de matar al mejor amante del mundo, pero supe que hay muchas leyes contra el suicidio." - Bob Patiño</p></div>
-        </article>
-    );
+const ComponenteFrase = ({ personajeProps }) => {
+  return (
+    <>
+      <h2>{personajeProps.character}</h2>
+      <article className="d-flex flex-row p-5 mb-2 contenedorFrasePersonaje">
+        <div>
+          <img
+            className="imagenPersonaje img-fluid"
+            src= {personajeProps.image}
+            alt={personajeProps.character}
+          />
+        </div>
+        <div className="mt-5 ms-5">
+          <p>{personajeProps.quote}</p>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default ComponenteFrase;
